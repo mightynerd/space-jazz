c737ddeb382117d075796453f7e3d2ab5257b42a
WPBack <daniel.a.back@telia.com>
2014-05-14 13:58:09 +0200

    file manager and user not finnished

---
+++ space-jazz/src/FileManager.java
@@ -0,0 +1,8 @@
+
+public class FileManager
+{
+	public FileManager()
+	{
+		
+	}
+}
---
+++ space-jazz/src/User.java
@@ -0,0 +1,12 @@
+
+public class User
+{
+	public String userName;
+	public String password;
+	public int points;
+	
+	public User()
+	{
+		points = 0;
+	}
+}
