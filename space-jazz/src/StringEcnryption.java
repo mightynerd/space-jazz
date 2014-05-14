8cc357e89f7d5f2bbaa6e2a375460f2b6f591a8e
Martin64 <martin.nilstomt1@gmail.com>
2014-05-15 00:17:22 +0200

    ecryption not working
    gives error when encrypting

--- space-jazz/src/StringEcnryption.java
+++ space-jazz/src/StringEcnryption.java
@@ -1,29 +1,59 @@
+//
+//	Project space-jazz
+//	Licensed under the General Public License v2
+//
+//	StringEncryption.java
+//	Encrypt/Decrypt strings
+//
+
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-//
-//	Project space-jazz
-//	Licenced under the Genereal Public Licence v2
-//
-//	InputManager.java
-//	Registers keyboard input for supported keys, supports multiple keys at once.
-//
 
+public class StringEcnryption {	
 
-public class StringEcnryption {
-	public void Ecnrypt(String Input, String Pass) {
-		byte[] InputByte = Input.getBytes(Charset.forName("UTF-8"));
-		//Hash to encrypt with from specified password
-		int PassHash = Pass.hashCode();
-		byte[] CryptoByte = ByteBuffer.allocate(4).putInt(PassHash).array();
-		//User specified password
-		byte[] KeyByte = Pass.getBytes(Charset.forName("UTF-8"));
+	static String IV = "SWAGOVERLOADJAZZ";
+	
+	 public static String Encrypt(String Input, String EncryptionKey) {
+		byte[] cipherByte = null;
 		
-		SecretKeySpec key = new SecretKeySpec(KeyByte, "DES");
-		IvParameterSpec ivSpec = new IvParameterSpec(CryptoByte);
+		try{
+			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+			SecretKeySpec key = new SecretKeySpec(EncryptionKey.getBytes("UTF-8"), "AES");
+			cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+			
+			cipherByte = cipher.doFinal(Input.getBytes("UTF-8"));
+			
+		}catch(Exception ex){
+			System.out.println(ex.toString());
+		}
 		
+		return new String(cipherByte);	
+		
+	 }
+	 
+	 public static String Decrypt(String Input, String EncryptionKey) throws Exception{
+		byte[] cipherInput = Input.getBytes("UTF-8");
+				
+		Cipher cipher = null;
+		
+		try{
+			cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+			SecretKeySpec key = new SecretKeySpec(EncryptionKey.getBytes("UTF-8"), "AES");
+			cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+		}
+		
+		return new String(cipher.doFinal(cipherInput),"UTF-8");
 	}
+	 
+	
+	
 }
