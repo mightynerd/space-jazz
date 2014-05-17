86caaef620bf3fb38b047869ca6aa1a62576c6ae
WPBack <daniel.a.back@telia.com>
2014-05-17 16:36:38 +0200

    FileManager now encrypts files

--- space-jazz/src/FileManager.java
+++ space-jazz/src/FileManager.java
@@ -1,6 +1,9 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,6 +13,8 @@ import javax.xml.bind.Unmarshaller;
 
 public class FileManager
 {
+	StringEncryptor enc = new StringEncryptor();
+	
 	public FileManager()
 	{
 
@@ -19,19 +24,27 @@ public class FileManager
 	{
 		try
 		{
-			File usersFile = new File(System.getProperty("user.dir") + "\\users.bin");
+			FileOutputStream usersFile = new FileOutputStream(System.getProperty("user.dir") + "\\users.bin");
+			StringWriter sw = new StringWriter();
+			byte[] encXML = null;
 			JAXBContext jaxbContext = JAXBContext.newInstance(Users.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 			
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(users, sw);
+				
+			enc.ObjectCrypter("1863189616648964");
+			encXML = enc.Encrypt(sw.toString());
+			System.out.println(sw.toString());
 			
-			jaxbMarshaller.marshal(users, usersFile);
-			jaxbMarshaller.marshal(users, System.out);
+			usersFile.write(encXML);
+			usersFile.close();
 		}
 		
-		catch (JAXBException e)
+		catch (JAXBException | IOException e)
 		{
-			System.out.println(e.toString());
+			System.out.println(e.getMessage());
 		}
 	}
 	
@@ -40,14 +53,23 @@ public class FileManager
 		try
 		{
 			File usersFile = new File(System.getProperty("user.dir") + "\\users.bin");
+			FileInputStream inputStream = new FileInputStream(System.getProperty("user.dir") + "\\users.bin");
+			byte[] encXML = new byte[(int) usersFile.length()];
+			String unencXML;
 			JAXBContext jaxbContent = JAXBContext.newInstance(Users.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContent.createUnmarshaller();
-			Users users = (Users) jaxbUnmarshaller.unmarshal(usersFile);
+			
+			inputStream.read(encXML);
+			inputStream.close();
+			unencXML = enc.Decrypt(encXML);
+			StringReader sr = new StringReader(unencXML);
+			Users users = (Users) jaxbUnmarshaller.unmarshal(sr);
+
 			
 			return users;
 		}
 		
-		catch (JAXBException e)
+		catch (JAXBException | IOException e)
 		{
 			System.out.println(e.toString());
 			return null;
@@ -56,17 +78,18 @@ public class FileManager
 	
 	public static void main(String[] args)
 	{
-		//User u1 = new User("Daniel", "Staples");
-		//User u2 = new User("Stas", "Kollegieblock");
-		//Users us = new Users();
+		User u1 = new User("Daniel", "Staples");
+		User u2 = new User("Stas", "Kollegieblock");
+		Users us = new Users();
 		
-		//us.users.add(u1);
-		//us.users.add(u2);
-		
-		//FileManager f = new FileManager();
-		//f.writeFile(us);
+		us.users.add(u1);
+		us.users.add(u2);
 		
 		FileManager f = new FileManager();
-		f.readFile();
+		f.writeFile(us);
+		
+		
+		us = f.readFile();
+		System.out.println(us.users.get(0).userName);
 	}
 }
