17f417cf7beedb94a82f4d6942a1866d6cba2eb0
WPBack <daniel.a.back@telia.com>
2014-05-28 13:12:59 +0200

    Added variables to User

--- space-jazz/src/StatTrack.java
+++ space-jazz/src/StatTrack.java
@@ -2,5 +2,7 @@
 public class StatTrack {
 
 	public final static int ASTEROID_CRASH_DAMAGE = 33;
+	public final static int ARMOR_MULTIPLIER = 100;
+	public final static int WEAPON_MULTIPLIER = 20;
 	
 }
--- space-jazz/src/User.java
+++ space-jazz/src/User.java
@@ -7,16 +7,44 @@ public class User
 	public String userName;
 	public String password;
 	public int points;
+	public int money;
+	public int weaponLevel;
+	public int armorLevel;
+	public int currentHealth;
 	
 	public User()
 	{
 		points = 0;
+		money = 0;
+		weaponLevel = 1;
+		armorLevel = 1;
+		currentHealth = getMaxHealth();
 	}
 	
 	public User(String name, String passwd)
 	{
 		points = 0;
+		money = 0;
+		weaponLevel = 1;
+		armorLevel = 1;
+		currentHealth = getMaxHealth();
 		userName = name;
 		password = passwd;
+	}
+	
+	public int getMaxHealth()
+	{
+		return armorLevel * StatTrack.ARMOR_MULTIPLIER;
+	}
+	
+	public int getWeaponDamage()
+	{
+		return weaponLevel * StatTrack.WEAPON_MULTIPLIER;
+	}
+	
+	public void addPoint(int point)
+	{
+		points += point;
+		money += point;
 	}
 }
