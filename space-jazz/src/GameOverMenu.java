26a419d450c4378a6583f8d3c0b44fb05182729a
Martin64 <martin.nilstomt1@gmail.com>
2014-06-04 13:51:40 +0200

    GameOver Screen

--- space-jazz/src/GameOverMenu.java
+++ space-jazz/src/GameOverMenu.java
@@ -6,13 +6,18 @@ public class GameOverMenu {
 	private User user;
 	MenuButton RestartButton;
 	private StateManager stateManager;
+	Sprite backGround;
 	
 	public GameOverMenu(StateManager stateMan, User user)
 	{
 		this.user = user;
 		this.stateManager = stateMan;
-		RestartButton = new MenuButton(100, 100, "Restart", 30);
+		RestartButton = new MenuButton(540, 500, "Restart", 30);
 		RestartButton.SetActive(true);
+		RestartButton.SetTextPos(new Vector2D(60, 60));
+		
+		backGround = new Sprite(0, 0);
+		backGround.AddTexture("Space pixel.png");
 	}
 	
 	public void Update(InputManager inputManager)
@@ -25,9 +30,11 @@ public class GameOverMenu {
 	
 	public void Draw(Renderer renderer)
 	{
-		renderer.DrawString("GameOver", 485, 250, Color.white, 70);
-		renderer.DrawString("Points: " + user.points, 100, 60, Color.white, 30);
-		renderer.DrawString("Player: " + user.userName, 100, 60, Color.white, 30);
+		backGround.Draw(renderer);
+		
+		renderer.DrawString("GameOver", 485, 235, Color.red, 70);
+		renderer.DrawString("Points: " + user.points, 540, 330, Color.white, 30);
+		renderer.DrawString("Player: " + user.userName, 540, 370, Color.white, 30);
 		RestartButton.Draw(renderer);
 	}
 
